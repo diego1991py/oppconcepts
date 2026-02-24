9c3c968bd0d87262e37d58096edbd2335a20795d
Diego Valencia <91valenciadiego@gmail.com>
2026-02-23 22:35:05 -0500

    Add employee Class

---
+++ OppConcepts/OppConcepts.Backend/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Text;
+
+namespace OppConcepts.Backend;
+
+public abstract class Employee
+{
+
+    //Constructors
+
+    protected Employee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        IsActive = isActive;
+        BornDate = bornDate;
+        HireDate = hireDate;
+    }
+
+
+    //Properties
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public Date BornDate { get; set; } = null!;
+
+    public Date HireDate { get; set; } = null!;
+
+    //Methods
+
+    public override string ToString()
+    {
+        return $"{Id}\t{FirstName} {LastName}\n\tValue to pay: {GetValueToPay()}";
+    }
+
+    public abstract decimal GetValueToPay();
+}

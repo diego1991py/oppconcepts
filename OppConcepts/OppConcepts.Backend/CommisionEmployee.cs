ac2984360a00cbd6083389a106ca6ef1e844b51b
Diego Valencia <91valenciadiego@gmail.com>
2026-02-24 22:06:46 -0500

    Add CommisionEmployee Class

---
+++ OppConcepts/OppConcepts.Backend/CommisionEmployee.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace OppConcepts.Backend
+{
+    public class CommisionEmployee : Employee
+    {
+       float _commisionPercentaje;
+       decimal _sales;
+
+        public CommisionEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate, float commisionPercentaje, decimal sales) :
+            base(id, firstName, lastName, isActive, bornDate, hireDate)
+        {
+            CommisionPercentaje = commisionPercentaje;
+            Sales = sales;
+        }
+
+        public float CommisionPercentaje
+        {
+            get => _commisionPercentaje;
+            set => _commisionPercentaje = ValidateCommisionPercentaje(value);
+        }
+        public decimal Sales
+        { 
+            get => _sales;
+            set => _sales = ValidateSales(value);
+        }
+
+        public override decimal GetValueToPay() => (Sales * (decimal)CommisionPercentaje);
+
+        public override string ToString() => base.ToString() + $"\n\t" +
+            $"Commision Percentaje.: {CommisionPercentaje,18:P2}\n\t" +
+            $"Sales............: {Sales,20:C}";
+
+
+        private float ValidateCommisionPercentaje(float commisionPercentaje)
+        {
+            if (commisionPercentaje < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(commisionPercentaje), "Commision Percentaje  value must be between 0 and 1");
+            }
+            return  commisionPercentaje;
+        }
+
+        private decimal ValidateSales(decimal sales)
+        {
+            if (sales < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sales), "Sales  value must be greater than 0");
+            }
+            return sales;            
+        }
+       
+
+    }
+}
--- OppConcepts/OppConcepts.Frontend/Program.cs
+++ OppConcepts/OppConcepts.Frontend/Program.cs
@@ -11,14 +11,24 @@ try
 
     var employees = new List<Employee>();
     decimal payroll = 0;
+
     var employe1 = new SalaryEmployee(1010, "Diego", "Valecia", true, new Date(1991, 12, 18), new Date(2020, 1, 1), 2500000);
     employees.Add(employe1);
+
     var employe2= new SalaryEmployee(2020, "Samuel", "Valecia", true, new Date(2000, 5, 18), new Date(2024, 12, 1), 3000000);
     employees.Add(employe2);
+
     var employe3 = new HourlyEmployee(3030, "Matias", "Valecia", true, new Date(210, 11, 20), new Date(2025, 1, 1), 15000, 160);
     employees.Add(employe3);
+
     var employe4 = new HourlyEmployee(4040, "Laura", "Valecia", true, new Date(1999, 6, 25), new Date(2026, 2, 15), 10000, 200);
     employees.Add(employe4);
+
+    var employee5 = new CommisionEmployee(5050, "Andrea", "Martínes", true, new Date(1990, 12, 17), new Date(2019, 10, 10), 0.03f, 600000000);
+    employees.Add(employee5);
+
+    var employee6 = new CommisionEmployee(6060, "Jaime", "Montoya", true, new Date(1987, 10, 18), new Date(2015, 5, 15), 0.03f, 250000000);
+    employees.Add(employee6);
 
     foreach ( var employe in employees )
     {
